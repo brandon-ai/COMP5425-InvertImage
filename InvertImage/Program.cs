3b304b889419cc3d0a862325c45221e3a67169bd
brandon-ai <brandon.cabanilla@gmail.com>
2015-04-18 01:09:25 +1000

    initial

---
+++ InvertImage/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+namespace InvertImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String win1 = "Test Window"; //The name of the window
+            CvInvoke.cvNamedWindow(win1); //Create the window using the specific name
+
+            Image<Bgr, Byte> frame = new Image<Bgr, Byte>("lena.jpg");
+            
+            byte[, ,] data = frame.Data;
+            
+            //Invert each pixel in the image
+            for (int i = 0; i < frame.Rows; i++)
+            {
+                for (int j = 0; j < frame.Cols; j++)
+                {
+                    data[i, j, 0] = (byte)(255 - data[i, j, 0]);
+                    data[i, j, 1] = (byte)(255 - data[i, j, 1]);
+                    data[i, j, 2] = (byte)(255 - data[i, j, 2]);
+                }
+            }
+
+            frame.Data = data;
+            CvInvoke.cvShowImage(win1, frame); //Show the image   
+
+            CvInvoke.cvWaitKey(0);
+
+            CvInvoke.cvDestroyWindow(win1); //Destory the window
+        }
+    }
+}
